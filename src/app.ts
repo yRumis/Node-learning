9bb30b6d66551d3a001e320ee0730b604c6aad4e
ramon <ramon.silva109@.com>
2026-05-18 16:55:54 -0300

    refactor: app modified

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { type Request, type Response} from 'express'
+import clienteRoutes from './routes/cliente.routes.js'
+
 const app = express();
 app.use(express.json());
 
@@ -6,7 +8,7 @@ app.get('/',(req: Request, res: Response)=>{
     res.json({message: 'API organizada por camadas'})
 })
 
-
+app.use('/clientes', clienteRoutes)
 
 app.listen(3000, ()=>{
     console.log("estamos rodando normalmente")
